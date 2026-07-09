2e1a94afbd86dcbce583000da89af72540dfb3aa
iki1uc <entertrainer@web.de>
2026-07-10 00:23:04 +0200

    Add computeIKL function to process context

--- ikl.js
+++ ikl.js
@@ -1,1 +1,14 @@
+// IKL / ikl.js
+
+import { IKL_FORMULA } from "./formula.js";
+
+export function computeIKL(context) {
+
+    return {
+        formula: IKL_FORMULA,
+        input: context,
+        real: true,
+        status: "IKL-ready"
+    };
+}
 
