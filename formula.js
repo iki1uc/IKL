0e377e7d733142db48d1fb9f315ef3122eaa6cb5
iki1uc <entertrainer@web.de>
2026-07-10 00:22:46 +0200

    Add IKL formula to formula.js

--- formula.js
+++ formula.js
@@ -1,1 +1,18 @@
+// IKL / formula.js
+
+export const IKL_FORMULA = `
+dir(
+  mir(
+    doch(
+      egal(
+        mov(
+          nodi - (
+            use(target) user
+          ) move
+        ) dir
+      ) nir
+    ) user
+  )
+) = REAL
+`;
 
